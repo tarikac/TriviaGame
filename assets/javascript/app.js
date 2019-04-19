5d7bf938143e933cede250c89e1ae1f595601b7b
tarikac <tarika.common@gmail.com>
2019-04-18 23:06:05 -0500

    set up layout and start button and initial click.

---
+++ assets/javascript/app.js
@@ -0,0 +1,26 @@
+
+//loads whenthe page loads
+window.onload = function () {
+    $("#start_game").on("click", start);
+};
+
+var countNumber;
+var clockRun = false;
+var time = 30;
+var correct = 0;
+var wrong = 0;
+
+function start () {
+
+    $("#game_portal").text("Display Counter Here");
+}
+
+function gameQuestions () {
+
+
+
+}
+
+function timeConverter(t){
+    
+}
